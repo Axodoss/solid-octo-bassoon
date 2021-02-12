4a553b185ae8edc96dc1970a8ec7c2449261c0da
Lukas Heise <lukas.heise@gmail.com>
2021-02-12 12:39:41 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
 #include <gb/gb.h>
-#include <stdio.h>
 
 #include "types.h"
 
+// 8x8
+unsigned char SmileTile[] =
+{
+  0x7E,0x7E,0x81,0xFF,0xA5,0xFF,0x81,0xFF,
+  0x81,0xFF,0xBD,0xFF,0x81,0xFF,0x7E,0x7E
+};
+
 void main()
 {
-	u16 i = 0;
+	//DISPLAY_ON;
+
+	set_sprite_data(0, 2, SmileTile);
+	set_sprite_tile(0, 0);
+	move_sprite(0, 88, 78);
+	SHOW_SPRITES;
+
+
 	while (1)
 	{
-		printf("Counter: %d\n", i++);
+		delay(1000);
+		scroll_sprite(0, 10, 0);
 	}
 }
