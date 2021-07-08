4c54ebb2fd3177b53d1c3d4ab4e1ed02b2b9cd27
Lukas Heise <lukas.heise@gmail.com>
2021-07-08 22:03:04 +0200

    Update types.h

--- src/types.h
+++ src/types.h
@@ -2,47 +2,84 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-#define MAX_ENTITY 16
+#define MAX_ENTITIES 32
+#define MAX_PARTICLES 16
+
+#define UPGRADE_WEAPON1 0
+#define UPGRADE_WEAPON2 1
+#define UPGRADE_WEAPON3 2
+#define UPGRADE_WEAPON4 4
+#define UPGRADE_SHIELD 8
+#define UPGRADE_RES1 16
+#define UPGRADE_RES2 32
 
 #define INTERNAL static inline
 
-typedef char i8;
-typedef short i16;
+typedef INT8 i8;
+typedef INT16 i16;
 
-typedef unsigned char u8;
-typedef unsigned short u16;
+typedef UINT8 u8;
+typedef UINT16 u16;
 
 typedef void (*EmptyCallback)();
 
+// static entity information
 typedef struct
 {
-	i16 Health;
+	u16 Health;
+	// TODO: sprite + animation
+	// TODO: behaviour
+} entity_definition_t;
+
+// enemy, powerup, trigger
+typedef struct
+{
+	u8 Type;
+	u16 Health;
+	i16 Position[2];
+} entity_t;
+
+// bullet, debry, explosion
+typedef struct
+{
+	u8 Type;
+	i16 Position[2];
+
+	// velocity, behaviour, damage
+} particle_t;
+
+// our hero
+typedef struct
+{
+	u8 Health;
 
 	i16 Position[2];
 	i16 Velocity[2];
 	i16 Acceleration[2];
 
-	// size, collision callback, sprite, etc.
+	u8 FireDisabled; // weapon fire counter, 0 = player can fire the weapon
 
-} entity_t;
-
-typedef struct
-{
-	u8 EntityId;
-
+	u16 Upgrades;
+	u16 State; // airborn, ...
 } player_t;
 
+// all the global values in the game in a single object
 typedef struct
 {
 	EmptyCallback MainLoop;
 
-	UINT8 Input;
-	UINT8 LastInput;
+	u8 Input;
+	u8 LastInput;
 
-	entity_t Entities[MAX_ENTITY];
+	entity_t Entities[MAX_ENTITIES];
 	u8 NumberOfEntities;
 
+	particle_t Particles[MAX_PARTICLES];
+	u8 NumberOfParticles;
+
 	player_t Player;
+
+	u16 StateVariable; // used by splash, main menu, intro and ending to determine the animation state, etc.
 
 } game_context_t;
 
